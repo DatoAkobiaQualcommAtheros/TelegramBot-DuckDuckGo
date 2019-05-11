abc9a86b9c063fb237f7db19928b3d92ca245f7c
DatoAkobiaQualcommAtheros <davidakobia3@gmail.com>
2019-05-12 00:47:49 +0400

    Greeting command /start

--- basis_of_bot/test.py
+++ basis_of_bot/test.py
@@ -1,30 +1,7 @@
-# -*- coding: utf-8 -*-
-
-import telebot
-from telebot import types
-import requests
-from keys import token
-
-# DevMode
-bot = telebot.TeleBot(token)
-
-
-@bot.message_handler(commands=['start'])
-def greetings(message):
-    markup = types.InlineKeyboardMarkup()
-    dev_git = types.InlineKeyboardButton(text='Source Code', url='https://github.com/DatoAkobiaQualcommAtheros'
-                                                                 '/TelegramBot-DuckDuckGo')
-    markup.add(dev_git)
-    bot.send_message(
-        message.from_user.id,
-        "Hi ^^ You can enter any query and I will try to find information about it, Ok? Let's go!",
-        reply_markup=markup
-    )
-
-
+"""
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    # wrapping in try:except to catch unexpected errors, for example: JSONDecodeError
+    #wrapping in try:except to catch unexpected errors, for example: JSONDecodeError
     try:
         response = requests.get(
             'http://api.duckduckgo.com/',
@@ -46,7 +23,5 @@ def get_text_messages(message):
         bot.send_photo(message.from_user.id, related_topics_icon)
     bot.send_message(message.from_user.id, text)
     bot.send_photo(message.from_user.id, image_url)
-
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    
+"""
